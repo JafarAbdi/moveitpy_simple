bc1cf9731c136a03c1d196b168dde647ffb707ce
JafarAbdi <jafar.uruc@gmail.com>
2023-06-21 23:36:45 +0100

    Add an example for using the visualizer without MoveItPySimple

--- example/visualizer.py
+++ example/visualizer.py
@@ -2,19 +2,23 @@
 
 """A simple example of using moveitpy with panda3d_viewer and wandb."""
 
+from copy import deepcopy
 from pathlib import Path
 
-import wandb
+from ament_index_python.packages import get_package_share_path
+from moveit.core.robot_model import RobotModel
 from moveit.core.robot_state import RobotState
 
+import wandb
 from moveitpy_simple.moveit_configs_utils import MoveItConfigsBuilder
+from moveitpy_simple.moveit_configs_utils.file_loaders import load_xacro
 from moveitpy_simple.moveitpy import MoveItPySimple
 from moveitpy_simple.moveitpy.visualization import Visualizer
 
 dir_path = Path(__file__).parent.absolute()
 
 
-# TODO: This won't work for an installed package
+# TODO: This won't work for an installed package when run from ros2 run ...
 moveit_configs = (
     MoveItConfigsBuilder(
         package=dir_path.parent
@@ -27,6 +31,8 @@ moveit_configs = (
     .to_moveit_configs()
 )
 
+
+# Using MoveItPySimple for planning
 moveitpy = MoveItPySimple(
     "moveitpy_simple",
     moveit_configs,
@@ -35,7 +41,7 @@ moveitpy = MoveItPySimple(
 
 robot_state = RobotState(moveitpy.robot_model)
 robot_state.set_to_default_values(moveitpy.arm.joint_model_group, "ready")
-robot_state.set_to_default_values(moveitpy.gripper.joint_model_group, "close")
+robot_state.set_to_default_values(moveitpy.gripper.joint_model_group, "open")
 robot_state.update()
 
 visualizer = Visualizer(
@@ -49,6 +55,36 @@ plan = moveitpy.arm.plan()
 if plan:
     visualizer.visualize_robot_trajectory(plan.trajectory)
 
+# Without MoveItPySimple by creating a robot state with a robot model
+#
+urdf_file = load_xacro(
+    get_package_share_path("moveit_resources_panda_moveit_config")
+    / "config"
+    / "panda.urdf.xacro",
+)
+srdf_file = load_xacro(
+    get_package_share_path("moveit_resources_panda_moveit_config")
+    / "config"
+    / "panda.srdf",
+)
+
+robot_model = RobotModel(urdf_xml_string=urdf_file, srdf_xml_string=srdf_file)
+robot_state = RobotState(robot_model)
+robot_state.set_to_default_values("panda_arm", "ready")
+robot_state.set_to_default_values("hand", "open")
+robot_state.update()
+
+robot_trajectory = [deepcopy(robot_state)]
+
+robot_state.set_to_default_values("panda_arm", "extended")
+robot_state.set_to_default_values("hand", "close")
+robot_state.update()
+
+robot_trajectory.append(deepcopy(robot_state))
+
+
+visualizer.visualize_robot_state(robot_state)
+visualizer.visualize_robot_trajectory(robot_trajectory)
 
 wandb.init(
     project="moveitpy_wandb",
